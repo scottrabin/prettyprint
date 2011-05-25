3e0e3ea6f96fc9491f36d535d0ab0e36d8aedaa8
Scott Rabin <scottrabin@gmail.com>
2011-05-25 13:12:38 -0400

    Added optional blacklist to prettyprint

--- prettyprint.js
+++ prettyprint.js
@@ -12,19 +12,21 @@ function retType( obj, depth, indent ){
  * @param {Number} [depth] The depth to truncate recursive calls to (to avoid circular JSON objects)
  * @param {Number} [indent] The indent level of this call to prettyprint. Inserts this number of
  *                 INDENT_VALUE strings after newlines.
+ * @param {Array<String>} [blacklist] An optional blacklist of object properties to not print
  * @returns {String} the string representing the pretty printed version of the input {@code obj}.
  */
-exports.prettyprint = function( obj, depth, indent ){
+exports.prettyprint = function( obj, depth, indent, blacklist ){
     // handle depth & indent arguments
     depth = ( isNaN( parseInt( depth ) ) ? exports.DEFAULT_DEPTH : depth );
     indent = ( isNaN( parseInt( indent ) ) ? 0 : indent );
+    blacklist = ( blacklist instanceof Array ? blacklist : [] );
 
     // get type
     var type = retType( obj );
 
     // dispatch on type
     var fn = ( depth < 0 ? exports.truncate : exports.format );
-    return ( fn[ type ] || fn.default )( obj, depth, indent );
+    return ( fn[ type ] || fn.default )( obj, depth, indent, blacklist );
 };
 
 /**
@@ -53,10 +55,10 @@ exports.ARRAY_INLINE_THRESHOLD = 1;
  * format function.
  */
 exports.format = {
-    '[object Array]' : function( obj, depth, indent ){
+    '[object Array]' : function( obj, depth, indent, blacklist ){
 	var r = [], contains_multiline = false, v, multiline = /[\r\n]/;
 	for( var i = 0, l = obj.length ; i < l ; ++i ){
-	    r[i] = exports.prettyprint( obj[i], depth - 1, indent + 1 );
+	    r[i] = exports.prettyprint( obj[i], depth - 1, indent + 1, blacklist );
 	    if( multiline.test( r[i] ) ){
 		contains_multiline = true;
 	    }
@@ -69,11 +71,11 @@ exports.format = {
 	    return "[\n" + ind + exports.INDENT_VALUE + r.join( ",\n" + ind + exports.INDENT_VALUE ) + "\n" + ind + "]";
 	}
     },
-    '[object Object]': function( obj, depth, indent ){
+    '[object Object]': function( obj, depth, indent, blacklist ){
 	var r = [], contains_multiline = false, v, multiline = /[\r\n]/;
 	for( var prop in obj ){
-	    if( obj.hasOwnProperty(prop)){
-		r.push( prop + ": " + exports.prettyprint( obj[prop], depth - 1, indent + 1 ) );
+	    if( obj.hasOwnProperty(prop) && ( blacklist.indexOf( prop ) === -1 ) ){
+		r.push( prop + ": " + exports.prettyprint( obj[prop], depth - 1, indent + 1, blacklist ) );
 		if( multiline.test( r[ r.length - 1 ] ) ){
 		    contains_multiline = true;
 		}
